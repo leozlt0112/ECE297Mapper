fa8204f521feb2e8a6a53cd68ef28d6cef27a149
Letian Zhang <letian.zhang@mail.utoronto.ca>
2020-02-17 14:41:53 -0500

    implemented draw_map_blank_canvas() in draw_map()
    Former-commit-id: 3c261d21c22ba5ef75630559fb2da95c41046faf

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -1,4 +1,5 @@
 /*
+ * 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -10,6 +11,7 @@
 #include "ezgl/graphics.hpp"
 
 void draw_map () {
+    draw_map_blank_canvas();
     return;
 }
 
