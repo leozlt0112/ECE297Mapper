7ead6a2c70b4afd10799d5616c51e52de17607ef
Dave Wang <vulpes.wang@mail.utoronto.ca>
2020-02-17 14:04:53 -0500

    implement draw_map window creation
    Former-commit-id: 9e3fa45cff10cb0efdc093d26fec9ecc4415f507

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -5,7 +5,20 @@
  */
 
 #include "m2.h"
+#include "ezgl/application.hpp"
+#include "ezgl/graphics.hpp"
 
-void draw_map() {
-    return;
+void draw_map () {
+  ezgl::application::settings settings; 
+  settings.main_ui_resource = 
+           "libstreetmap/resources/main.ui"; 
+  settings.window_identifier = "MainWindow"; 
+  settings.canvas_identifier = "MainCanvas";
+
+  ezgl::application application(settings); 
+
+
+
+  application.run(nullptr, nullptr,
+                  nullptr, nullptr);
 }
