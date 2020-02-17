6a807817f4b1abdea081ac4e1be45571dbf2691f
Qianhan Cui <qianhan.cui@mail.utoronto.ca>
2020-02-17 09:35:48 -0500

    created m2.cpp
    Former-commit-id: 17182a2b193a1de0ee622ea60140e4a5536a9b46

--- libstreetmap/src/m2.cpp
+++ libstreetmap/src/m2.cpp
@@ -4,3 +4,18 @@
  * and open the template in the editor.
  */
 
+#include "m2.h"
+#include "m1.h"
+#include "m1_more.h"
+#include "StreetsDatabaseAPI.h"
+#include "math.h"
+#include <map>
+#include <algorithm>    // std::sort
+#include "OSMDatabaseAPI.h"
+
+// Draws the currently loaded map.
+// draw_map () assumes the map has already been loaded with load_map ()
+// before it is called. The ece297exercise unit tests always call 
+// load_map (string map_name) before calling draw_map. Your main () program
+// should follow the same convention.
+void draw_map();
