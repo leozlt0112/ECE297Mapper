93fb5dfadf8f3323fb4b79892e2e947c847ef911
Qianhan Cui <qianhan.cui@mail.utoronto.ca>
2020-03-13 12:48:38 -0400

    implemented compute_path_walking-time, but not tested yet
    Former-commit-id: 0dedeeebe556e08960ec26bee51867015a763df8

--- libstreetmap/src/m3.cpp
+++ libstreetmap/src/m3.cpp
@@ -1,7 +1,3 @@
-#include "m3.h"
-#include "m2.h"
-#include "m2_more.h"
-
 <<<<<<< HEAD
 #include "m3.h"
 #include "m3_more.h"
@@ -79,19 +75,16 @@ std::vector<StreetSegmentIndex> find_path_between_intersections(
 double compute_path_walking_time(const std::vector<StreetSegmentIndex>& path, 
                                  const double walking_speed, 
                                  const double turn_penalty){  
-    double temp = 0.0;
-    return temp;
-    /* int turns; 
+    double walking_time = 0.0;
+    double seg_walking_time = 0.0;
+    int turns = 0; 
    //empty vector
     if(path.size() == 0){
         return 0;
     }
-    
-    //The walking time is the sum of the length/<walking_speed> for each street segment, plus
-    // the given turn penalty, in seconds, per turn implied by the path
-    
+       
     //count turns in the path (whenever the street id changes) 
-    for(int i=0; i< path.size(); ++i){
+    for(int i=0; i< (path.size()-1); ++i){
         InfoStreetSegment this_Seg_info = getInfoStreetSegment (path[i]);
         InfoStreetSegment next_Seg_info = getInfoStreetSegment (path[i+1]);
         
@@ -100,21 +93,16 @@ double compute_path_walking_time(const std::vector<StreetSegmentIndex>& path,
             turns = turns +1;
         }
     }
-    */
+       
+    //adds up walking time for all the segs in the path
+    for(int segCount=0; segCount< path.size(); ++segCount){       
+        seg_walking_time += (streetSeg_length[path[segCount]] / walking_speed);
+    }
+    
+        
+    //The walking time is the sum of the length/<walking_speed> for each street segment, plus
+    // the given turn penalty, in seconds, per turn implied by the path
+    walking_time =  seg_walking_time + (turns*turn_penalty);
+    return walking_time;
 }
 
-
-
-
-std::pair<std::vector<StreetSegmentIndex>, std::vector<StreetSegmentIndex> 
-         find_path_with_walk_to_pick_up(
-                          const IntersectionIndex start_intersection, 
-                          const IntersectionIndex end_intersection,
-                          const double turn_penalty,
-                          const double walking_speed, 
-                          const double walking_time_limit){
-    std::pair<std::vector<StreetSegmentIndex> temp1;
-    std::pair<std::vector<StreetSegmentIndex> temp2;
-    return temp1, temp2;
-}
->>>>>>> commit to resolve conflict
