a0574aca673174c310988b2f3d6fd6fcefe6d7ac
Qianhan Cui <qianhan.cui@mail.utoronto.ca>
2020-03-13 19:16:16 -0400

    pasted exerciser for computing walking_time.
    Former-commit-id: 2290c2c55332f10501be1b164d61cb21017aa854

--- libstreetmap/src/m3.cpp
+++ libstreetmap/src/m3.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "m3.h"
 #include "m3_more.h"
 
@@ -44,12 +43,46 @@ void pathFind_load(){
         nodes[i].idx_pnt = i;
     }
 }
-=======
 
+// Returns the time required to travel along the path specified, in seconds.
+// The path is given as a vector of street segment ids, and this function can
+// assume the vector either forms a legal path or has size == 0.  The travel
+// time is the sum of the length/speed-limit of each street segment, plus the
+// given turn_penalty (in seconds) per turn implied by the path.  If there is
+// no turn, then there is no penalty. Note that whenever the street id changes
+// (e.g. going from Bloor Street West to Bloor Street East) we have a turn.
 double compute_path_travel_time(const std::vector<StreetSegmentIndex>& path, 
                                 const double turn_penalty){
-    double temp = 0.0;
-    return temp;
+    //double temp = 0.0;
+    //return temp;
+    double travel_time = 0.0;
+    double seg_travel_time = 0.0;
+    int turns = 0; 
+    //empty vector
+    if(path.size() == 0){
+        return 0;
+    }
+    
+    //count turns in the path (whenever the street id changes) 
+    for(int i=0; i< (path.size()-1); ++i){
+        InfoStreetSegment this_Seg_info = getInfoStreetSegment (path[i]);
+        InfoStreetSegment next_Seg_info = getInfoStreetSegment (path[i+1]);
+        
+        //calculate turns by comparing street id with its the next street id. 
+        if(this_Seg_info.streetID != next_Seg_info.streetID){
+            turns = turns +1;
+        }
+    }
+     
+    //adds up travel time for all the segs in the path
+    for(int segCount=0; segCount< path.size(); ++segCount){       
+        seg_travel_time += (streetSeg_length[path[segCount]] / streetSegments[path[segCount]].speedLimit);
+    }    
+        
+    //The travel time is the sum of the length/speed-limit of each street segment, plus the
+    // given turn_penalty (in seconds) per turn implied by the path. 
+    travel_time =  seg_travel_time + (turns*turn_penalty);
+    return travel_time;
 }
 
 
@@ -65,13 +98,6 @@ std::vector<StreetSegmentIndex> find_path_between_intersections(
 
 
 
-// Returns the time required to "walk" along the path specified, in seconds.
-// The path is given as a vector of street segment ids. The vector can be of
-// size = 0, and in this case, it the function should return 0. The walking
-// time is the sum of the length/<walking_speed> for each street segment, plus
-// the given turn penalty, in seconds, per turn implied by the path. If there
-// is no turn, then there is no penalty.  As mentioned above, going from Bloor
-// Street West to Bloor street East is considered a turn
 double compute_path_walking_time(const std::vector<StreetSegmentIndex>& path, 
                                  const double walking_speed, 
                                  const double turn_penalty){  
@@ -97,8 +123,7 @@ double compute_path_walking_time(const std::vector<StreetSegmentIndex>& path,
     //adds up walking time for all the segs in the path
     for(int segCount=0; segCount< path.size(); ++segCount){       
         seg_walking_time += (streetSeg_length[path[segCount]] / walking_speed);
-    }
-    
+    }    
         
     //The walking time is the sum of the length/<walking_speed> for each street segment, plus
     // the given turn penalty, in seconds, per turn implied by the path
@@ -106,3 +131,34 @@ double compute_path_walking_time(const std::vector<StreetSegmentIndex>& path,
     return walking_time;
 }
 
+// This is an "uber pool"-like function. The idea is to minimize driving travel
+// time by walking to a pick-up intersection (within walking_time_limit secs)
+// from start_intersection while waiting for the car to arrive.  While walking,
+// you can ignore speed limits of streets and just account for given
+// walking_speed [m/sec]. However, pedestrians should also wait for traffic
+// lights, so turn_penalty applies to whether you are driving or walking.
+// Walking in the opposite direction of one-way streets is fine. Driving is
+// NOT!  The routine returns a pair of vectors of street segment ids. The first
+// vector is the walking path starting at start_intersection and ending at the
+// pick-up intersection. The second vector is the driving path from pick-up
+// intersection to end_interserction.  Note that a start_intersection can be a
+// pick-up intersection. If this happens, the first vector should be empty
+// (size = 0).  If there is no driving path found, both returned vectors should
+// be empty (size = 0). 
+// If the end_intersection turns out to be within the walking time limit, 
+// you can choose what to return, but you should not crash. If your user 
+// interface can call this function for that case, the UI should handle
+// whatever you return in that case.
+ std::pair<std::vector<StreetSegmentIndex>, std::vector<StreetSegmentIndex>> 
+         find_path_with_walk_to_pick_up(
+                          const IntersectionIndex start_intersection, 
+                          const IntersectionIndex end_intersection,
+                          const double turn_penalty,
+                          const double walking_speed, 
+                          const double walking_time_limit){
+     std::pair<std::vector<StreetSegmentIndex>, std::vector<StreetSegmentIndex>> temp;
+     std::vector<StreetSegmentIndex> temp1;
+     std::vector<StreetSegmentIndex> temp2;
+     temp = std::make_pair(temp1,temp2);
+     return temp;
+}
